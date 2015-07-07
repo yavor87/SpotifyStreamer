842dc1e00230754895c5ba798d71e0fe04491e55
Yavor Ivanov <yavor87@gmail.com>
2015-07-07 16:36:12 +0300

    Adding ArtistListFragment

---
+++ app/src/main/java/com/yavor/spotifystreamer/ArtistListFragment.java
@@ -0,0 +1,44 @@
+package com.yavor.spotifystreamer;
+
+
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class ArtistListFragment extends ListFragment {
+
+    public ArtistListFragment() {
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_artist_list, container, false);
+
+        EditText editText = (EditText) view.findViewById(R.id.search_bar);
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    performSearch(v.getText().toString());
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        return view;
+    }
+
+    void performSearch(String text) {
+        Log.v(this.getClass().getSimpleName(), text);
+    }
+}
